99bb795fd4d87773288eaa2465cb56e3a90bc64b
CodingNav <navleenkaurr@gmail.com>
2021-07-19 17:55:15 -0400

    Added ingredients API request

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+
+
+// Ingredients API Request
+function searchIngredients(ingredient) {
+    // Used a proxy to get rid of CORS error
+    var proxy = "https://api.allorigins.win/get?url=";
+    var ingredientsURL = proxy + "https://www.wholefoodsmarket.com/api/search?text=" + ingredient + "&store=10713&limit=60&offset=0";
+
+    fetch(ingredientsURL)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (proxyData) {
+            return JSON.parse(proxyData.contents);
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+searchIngredients("eggs");
+
