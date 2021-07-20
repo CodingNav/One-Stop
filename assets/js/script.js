8189e9906fda1d98b18425d1d5e843be82ffe0f3
CodingNav <navleenkaurr@gmail.com>
2021-07-19 21:07:43 -0400

    Added loop to grab data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 
-
 // Ingredients API Request
 function searchIngredients(ingredient) {
     // Used a proxy to get rid of CORS error
     var proxy = "https://api.allorigins.win/get?url=";
-    var ingredientsURL = proxy + "https://www.wholefoodsmarket.com/api/search?text=" + ingredient + "&store=10713&limit=60&offset=0";
+    var ingredientsURL = proxy + encodeURIComponent("https://www.wholefoodsmarket.com/api/search?text=" + ingredient + "&store=10713&limit=60&offset=0");
 
     fetch(ingredientsURL)
         .then(function (response) {
@@ -17,6 +16,15 @@ function searchIngredients(ingredient) {
         .then(function (data) {
             console.log(data);
         })
+
+    // Loops through data results and grabs the data 
+    for (i = 0; i < data.results.length; i++) {
+        var price = data.results[i].regularPrice;
+        var brand = data.results[i].brand;
+        var name = data.results[i].name;
+        var link = "https://www.wholefoodsmarket.com/product/" + data.results[i].slug;
+        var image = data.results[i].imageThumbnail;
+    }
 }
 
 searchIngredients("eggs");
