855da21ef4079b2c9f5af9ccee1b73ce31f7f841
arlenwh-code <arlenwh@protonmail.com>
2021-07-21 21:17:50 -0400

    finished dynamic recipe cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,47 +36,59 @@ function searchRecipe(recipe) {
         return response.json();
     }).then(function (data) {
         console.log(data);
-        console.log(data.meals[0]); //gets the data for the first recipe search result
+        console.log(data.meals[0].strMeal); //gets the data for the first recipe search result
+        console.log(data.meals.length);
+        recipeCard(data, data.meals.length, data.meals[0].strMeal);
     });
 };
 
-var rowDiv = document.createElement('div');
-var colDiv = document.createElement('div');
-var cardDiv = document.createElement('div');
-var cardImage = document.createElement('div');
-var cardContent = document.createElement('div');
-var Img = document.createElement('img');
-var a = document.createElement('a');
-var i = document.createElement('i');
-var span = document.createElement('span');
-var p = document.createElement('p');
+function recipeCard(data, length){
+    console.log(data); //data from recipe api is passed to this function and used to get the recipe names and recipe descriptions
+    console.log(length);
+    for (var x = 0; x < length; x++){
+        console.log("hello");
+        var rowDiv = document.createElement('div');
+        var colDiv = document.createElement('div');
+        var cardDiv = document.createElement('div');
+        var cardImage = document.createElement('div');
+        var Img = document.createElement('img');
 
-document.querySelector(".result-container").append(rowDiv);
-rowDiv.className = "r2 row ";
-document.querySelector(".r2").appendChild(colDiv);
-colDiv.className = "column2 col s12 m3";
-document.querySelector(".column2").appendChild(cardDiv);
-cardDiv.className = "card2 card";
+        var a = document.createElement('a');
+        var i = document.createElement('i');
+        var cardContent = document.createElement('div');
+        var span = document.createElement('span');
+        var p = document.createElement('p');
 
-
-document.querySelector(".card2").appendChild(cardImage);
-cardImage.className = "cardImage2 card-image";
-document.querySelector(".cardImage2").appendChild(Img);
-Img.src = "./assets/images/sample-pic.jpg";
-document.querySelector(".cardImage2").appendChild(a);
-a.className = "btn-f2 btn-floating halfway-fab waves-effect waves-light red";
-document.querySelector(".btn-f2").appendChild(i);
-i.className = "far2 far fa-heart";
+        document.querySelector(".result-container").append(rowDiv);
+        rowDiv.className = "r" + [x] + " row";
+        document.querySelector(".r" + [x]).appendChild(colDiv);
+        colDiv.className = "column" + [x] + " col s12 m3";
+        document.querySelector(".column" + [x]).appendChild(cardDiv);
+        cardDiv.className = "card" + [x] + " card";
 
 
-document.querySelector(".card2").appendChild(cardContent);
-cardContent.className = "cardContent2 card-content";
-document.querySelector(".cardContent2").appendChild(span);
-span.className = "cT2 card-title";
-document.querySelector(".cT2").textContent = "RecipeName";
-document.querySelector(".cardContent2").appendChild(p);
-p.className = "p2";
-document.querySelector(".p2").textContent = "Description";
+        document.querySelector(".card" + [x]).appendChild(cardImage);
+        cardImage.className = "cardImage" + [x] + " card-image";
+        document.querySelector(".cardImage" + [x]).appendChild(Img);
+        Img.className = "img" + [x];
+        Img.src = data.meals[x].strMealThumb;
+        document.querySelector(".cardImage" + [x]).appendChild(a);
+        a.className = "btn-f" + [x] + " btn-floating halfway-fab waves-effect waves-light red";
+        document.querySelector(".btn-f" + [x]).appendChild(i);
+        i.className = "far" + [x] + " far fa-heart";
+
+
+        document.querySelector(".card" + [x]).appendChild(cardContent);
+        cardContent.className = "cardContent" + [x] + " card-content";
+        document.querySelector(".cardContent" + [x]).appendChild(span);
+        span.className = "cT" + [x] + " card-title";
+        document.querySelector(".cT" + [x]).textContent = data.meals[x].strMeal; //name of meal
+        document.querySelector(".cardContent" + [x]).appendChild(p);
+        p.className = "p" + [x];
+        document.querySelector(".p" + [x]).textContent = data.meals[x].strCategory; //Description of meal
+        
+    };
+};
 
 
 searchRecipe("egg");
